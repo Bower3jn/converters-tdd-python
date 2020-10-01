9568c3e3342069d7d92a5386f482b574d1394d8e
Nick Bower <bower3jn@gmail.com>
2020-10-01 16:06:12 -0400

    pytest & pytest installation

---
+++ converters_test.py
@@ -0,0 +1,6 @@
+"""Unit test for the units converter library functions"""
+def describe_a_library_of_units_converters():
+    """test suite for units conversion function"""
+    # pylint: disable=unused-variable
+    def blows_smoke():
+        assert True
